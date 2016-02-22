34ab745316a6a4d5a35aec7be10a322f316743c5
HUBFACTORY <info@hubfactory.jp>
2016-02-22 17:36:21 +0900

    Added files via upload

---
+++ sampo/sampo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  sampo
+//
+//  Created by otamasayuki on 2016/02/22.
+//  Copyright © 2016年 hubfactory. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
